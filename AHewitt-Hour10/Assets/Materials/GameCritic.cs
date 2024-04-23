3935d0b3825aa1798cb0ef4246985dabb24b006a
Kokkiri1 <ahewittaj@gmail.com>
2024-04-22 18:24:06 -0700

    AHewittHour10

---
+++ AHewitt-Hour10/Assets/Materials/GameCritic.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCritic : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Added obstacle pillars, a casualty ball, higher walls and a camera above for minimap,");
+    }
+}
+
